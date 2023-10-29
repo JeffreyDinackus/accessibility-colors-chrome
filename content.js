43fae19669096fb511059ab0aa2c93da2ff29679
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-28 20:06:07 -0400

    adding more functionality

---
+++ content.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Your code to be executed on page load here.
+  document.body.style.backgroundColor = 'red'; // Change to the color you want.
+});
